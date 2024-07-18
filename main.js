60d7d7ffad54d7dab9bb42aa509ba21effcf46d3
Enrique1311 <ejspinelli131176@gmail.com>
2024-07-18 20:27:12 -0300

    Screen Object

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log(screen);
+
+console.log(screen.width + "px"); // total width of screen
+console.log(screen.height + "px"); // total height of screen
+console.log(screen.availWidth + "px"); // available width of screen
+console.log(screen.availHeight + "px"); // available height of screen
+console.log(screen.pixelDepth + "px"); // pixel depth of screen
+console.log(screen.colorDepth + "px"); // color depth of screen
